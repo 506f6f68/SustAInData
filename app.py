e7413b211b9c061f236907a5c0edb6789c2f6021
Chen-Hao Wu <howdywu@gmail.com>
2024-11-03 07:56:20 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Standard library imports
 import os
-from typing import Sequence
+from typing import Sequence, List, Optional
 from typing_extensions import Annotated, TypedDict
 
 # Third-party library imports
@@ -8,7 +8,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 # LangChain imports
@@ -18,6 +18,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
+from langchain_mistralai import ChatMistralAI
 from langchain_ollama.chat_models import ChatOllama
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -61,17 +62,46 @@ def get_summary_prompt():
     return (
         "You are tasked with summarizing the following chat history related to building a data center. "
         "Provide 3 different location and answer to the belowing questions in a concise manner."
-        "You should first answer the location, and then provide the answer to the question based on the chosen location."
+        "You should first describe the client's condition(tier, number of servers, budget)"
+        "For each locations, providing answers to the belowing questions based on the chosen location."
         "If you cannot get the answer from the context or from the chat history, offer a relevant answer based on industry knowledge and best practices."
-        "1. Cooling technology: Describe optimal options based on budget, considering both building and IT equipment."
-        "2. HVAC(Heating, Ventilation, AC cost), including basic cost(battery cost) and provide a list of specific suppliers"
+        "1. Cooling technology: Describe optimal options based on budget, considering both using for buildings and for servers."
+        "2. HVAC(Heating, Ventilation, AC cost), including basic cost(battery cost) and specifying suppliers"
         "3. Power supply and backup"
-        "4. Energy options in that location"
+        "4. Energy options in that location(grid, off-grid, mix)"
         "5. Energy efficiency design of that location"
+        "6. Estimated carbon emissions"
+        "7. Water usage"
+        "8. Regulatory compliance"
         "The summary should like a report, but do not mark the questions as questions, just provide the answers."
-        "Seperate each answer with 10 '-' characters."
-        "\n\n{context}"
         "\n\n"
+        "{context}"
+        "\n\n"
+    )
+
+
+def get_parsing_summary_prompt():
+    return (
+        "You are an expert extraction algorithm."
+        "Only extract relevant information from the text. "
+        "If you do not know the value of an attribute asked to extract, return an estimate based on the information provided.",
+        "Below are some formulas to help you extract the information:"
+        "how to calculate renewablePercentage: "
+        "<if all using grid, the renewablePercentage is 40%>."
+        "<if all using off-grid, the renewablePercentage is 100%>."
+        "<if using mix, the renewablePercentage is 0.4*(Percentage of gird) + 1*(1-Percentage of gird)>."
+        "how to calculate the cost of grid energy in different area: "
+        "<in Sacramento/Central Valley, the cost is $0.16/kWh>."
+        "<in Bay Area, the cost is $0.403/kWh>."
+        "<in Los Angeles, the cost is $0.28/kWh>."
+        "how to calculate the cost of off-grid energy in different area: "
+        "<in all areas, the cost of off-grid energy is $15/kWh>"
+        "how to calculate Carbon Emmisions using grid energy: "
+        "<in Sacramento, the emission is 265g CO2eq/kWh>."
+        "<in Bay Area/Central Valley, the emission is 261g CO2eq/kWh>."
+        "<in Los Angeles, the emission is 381g CO2eq/kWh>."
+        "how to calculate Carbon Emmisions using off-grid energy: "
+        "<in all areas, the emission is 0g CO2eq/kWh>.",
     )
 
 
@@ -116,7 +146,7 @@ def parse_external_data():
     # vector_store.add_documents(docs)
 
     print(f"starting to parse {len(pdf_paths)} pdfs...")
-    parsed_docs = [parse_pdf(path) for path in pdf_paths]
+    parsed_docs = [parse_pdf(path) for path in pdf_paths[:1]]
     docs = [page for pages in parsed_docs for page in pages]
     print("finished parsing, starting to add to vector store...")
     vector_store.add_documents(docs)
@@ -157,6 +187,37 @@ fastapi.add_middleware(
 )
 
 
+class summary_schema(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    Location: Optional[str] = Field(default=None, description="The location of the data center, specified as a city or region")
+    CoolingTech_air_name: Optional[str] = Field(default=None, description="The cooling technology used for buildings in the data center")
+    CoolingTech_air_supplier: Optional[str] = Field(default=None, description="The supplier of the cooling technology used for buildings in the data center")
+    CoolingTech_server_name: Optional[str] = Field(default=None, description="The cooling technology used for servers in the data center")
+    CoolingTech_server_supplier: Optional[str] = Field(default=None, description="The supplier of the cooling technology used for servers in the data center")
+    Energy_Choice: Optional[str] = Field(default=None, description="The energy choice for the data center(grid, off-grid, or mix)")
+    RenewablePercentage: Optional[str] = Field(default=None, description="The percentage of renewable energy used in the data center")
+    GridEnergyCost: Optional[str] = Field(default=None, description="The cost of grid energy in the data center")
+    OffGridEnergyCost: Optional[str] = Field(default=None, description="The cost of off-grid energy in the data center")
+    OperatingCost: Optional[str] = Field(default=None, description="The operating cost(utility, rental, hr and energy costs) of the data center")
+    CarbonEmissions: Optional[str] = Field(default=None, description="The carbon emissions of the data center")
+    WaterUsage: Optional[str] = Field(default=None, description="The water usage of the data center")
+    RegulatoryCompliance: Optional[str] = Field(default=None, description="The regulatory compliance of the data center")
+    
+class Plan(BaseModel):
+    """Extracted data about people."""
+
+    # Creates a model so that we can extract multiple entities.
+    plan: List[summary_schema]
+    
 @fastapi.post("/process_text/")
 async def process_text(input_data: dict):
     tier = input_data["tier"]
@@ -171,7 +232,10 @@ async def process_text(input_data: dict):
 
 @fastapi.get("/summary/", response_class=HTMLResponse)
 async def get_summary():
-    chat_history = app.get_state(config).values["chat_history"]
+    try:
+        chat_history = app.get_state(config).values["chat_history"]
+    except KeyError:
+        chat_history = []
     formatted_history = "\n".join(f"User: {msg.content}" if isinstance(msg, HumanMessage) else f"Bot: {msg.content}" for msg in chat_history)
     retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 5})
     rag_context = retriever.get_relevant_documents(formatted_history)
@@ -182,6 +246,12 @@ async def get_summary():
     combined_context = (Document(page_content=combined_context, metadata={"title": "chat_history"}),)
     summary = summary_chain.invoke({"context": combined_context})
     print(summary)
+    ## dashboard
+    # summary_extract_prompt = ChatPromptTemplate.from_messages([("system", get_parsing_summary_prompt()), ("human", "{text}")])
+    # summary_extractor = ChatMistralAI(model="mistral-large-latest", temperature=0)
+    # runnable = summary_extract_prompt | summary_extractor.with_structured_output(schema=Plan)
+    # res = runnable.invoke({"text": summary})
+    # print(res)
     return JSONResponse(content={"summary": summary})
 
 
